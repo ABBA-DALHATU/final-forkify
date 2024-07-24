ce948a378a0202afe2a99062c58d54ccef6610e1
ABBA-DALHATU <abbadalhat101@gmail.com>
2024-07-24 15:15:16 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '69e9f324-15ee-40d7-b3e5-2ad06c07b6e1';
+export const MODAL_CLOSE_SEC = 2.5;
